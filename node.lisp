3811daec3f08a6a27089fe8caeb20d520d2b2223
Patrick Stein <pat@nklein.com>
2008-04-28 23:31:38 -0500

    Added reader for node-pos

--- node.lisp
+++ node.lisp
@@ -2,7 +2,8 @@
 
 (defclass node ()
     ((name :initarg :name)
-     (pos  :initarg :pos  :initform (random-vector 'single-float 3))
+     (pos  :reader node-pos :initarg :pos
+	     :initform (random-vector 'single-float 3))
      (mass :initarg :mass :initform 1.0s0)
      prev
      acc))
